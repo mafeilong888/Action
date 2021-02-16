21b46c8730b8d5d9be0efa47f75f5ab726c0d451
willianmmiao <76838280+willianmmiao@users.noreply.github.com>
2021-02-16 10:05:14 +0800

    Update zhimaCOOKIE.js

--- Scripts/zhimaCOOKIE.js
+++ Scripts/zhimaCOOKIE.js
@@ -2,7 +2,7 @@
 
 //独立COOKIE文件     ck在``里面填写，多账号换行
 
-let zhimabodyVal= ``
+let zhimabodyVal= `appversion=10&desarr=B9UrvK6HqhbGjEVFP3eNKnGZOgPLMKoE0LqUPC7rhuvRBwh5rmsJ85GcyX87%2BNssL1EvonhUmpN07J1H6OW9MpP5Vvvl4Yq5CQairPt0x/19pZLIreGshvXGPjmzxk7Aj9a1s4jrU/77piilULuseMdoHQ7%2BmB/wsRS8gKJ%2BSmdVHwafj3RWTjNbxeOD/FX5ZWAacfk0ZOgPXuXmK9edHIRS6Wg/S8A3d7%2B%2BcIbsUyiiGO%2BvUzANt6nDL4UB852TUeFkbAf8jNPy4rKPEKDmEE5dSdfGwmFj%2BlgilncDu0GesTWDrBN7xwDrfhqD8UJUCv%2BVP7z%2B/CNol2pLO5TaIYKjo5E559uzkP836dWTtdzWnENwr0j/cqZyF2G5q3Exjc1VpAHOYCKDpzItZquVSalCmID9g8Jt4qMhLv2/iVVX3UMeC9JoR9ZBwfVHNfnGd4vGLcTwCMfxKU6DMG2cKO58koAlY4UvfGuMHB7XHhh97RNU4hhyefSyKoLKs3Phi/FwmvzqkZ1ukr6Mii5y7LhVOZ9/uGMZnFo`
 
 
 let zhimacookie = {
