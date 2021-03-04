eda8ee989ba956c017b7e7528db2fe5eb7f06bf9
willianmmiao <76838280+willianmmiao@users.noreply.github.com>
2021-03-04 08:57:01 +0800

    Update qmydCOOKIE.js

--- Scripts/qmydCOOKIE.js
+++ Scripts/qmydCOOKIE.js
@@ -1,6 +1,6 @@
 //独立COOKIE文件     ck在``里面填写，多账号换行
 
-let qmydtokenVal = ``
+let qmydtokenVal = `Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczpcL1wvcW15ZC55aWNoZW5ndy5jblwvYXBwc1wvbG9naW5cL3dlY2hhdCIsImlhdCI6MTYxNDgxNzY1NywiZXhwIjoyMDUzNjQxNzY1NywibmJmIjoxNjE0ODE3NjU3LCJqdGkiOiJNUEh5d2lkWE9wUHZnOUdFIiwic3ViIjoxMTU5OTcsInBydiI6IjQxZGY4ODM0ZjFiOThmNzBlZmE2MGFhZWRlZjQyMzQxMzcwMDY5MGMifQ.JvVMSZM3NTKt1tBBK1N8DOQSpStGVeruoFlKQcI1CGo`
 
 
 
