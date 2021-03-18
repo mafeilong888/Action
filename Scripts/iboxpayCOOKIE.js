cde234b93589777f372129c2cb146c556a4c2888
willianmmiao <76838280+willianmmiao@users.noreply.github.com>
2021-03-18 11:16:07 +0800

    Update iboxpayCOOKIE.js

--- Scripts/iboxpayCOOKIE.js
+++ Scripts/iboxpayCOOKIE.js
@@ -2,14 +2,21 @@
 
 //独立COOKIE文件     ck在``里面填写，多账号换行
 
-let refreshtokenVal= ``
+let refreshtokenVal= `5bc78a79c9794846b9d446f7aa094177
+677f8dd8680147d88cb5667fdee1a437
+587d894558d548399c4e92a9cd1d5ca8`
+let iboxpayvideoheaderVal= ``
+let iboxpayvideobodyVal= ``
 
 
 let iboxpaycookie = {
 
-  refreshtokenVal: refreshtokenVal,  
+  refreshtokenVal: refreshtokenVal, 
+    iboxpayvideoheaderVal: iboxpayvideoheaderVal,  
+      iboxpayvideobodyVal: iboxpayvideobodyVal,   
   
 }
 
 module.exports =  iboxpaycookie
   
+
