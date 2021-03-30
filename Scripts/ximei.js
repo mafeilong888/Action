120453d8b24dfaac5b7be088caf93d916eb9d724
willianmmiao <76838280+willianmmiao@users.noreply.github.com>
2021-03-30 14:53:02 +0800

    Update ximei.js

--- Scripts/ximei.js
+++ Scripts/ximei.js
@@ -61,15 +61,51 @@ const $ = new Env('西梅');
 let status;
 status = (status = ($.getval("ximeistatus") || "1") ) > 1 ? `${status}` : ""; // 账号扩展字符
 const ximeiurlArr = [], ximeihdArr = [],ximeicount = ''
-let times = Math.round(Date.now())
+let times = Math.round(Date.now()/ 1000)
 let ximeiurl = $.getdata('ximeiurl')
 let ximeihd = $.getdata('ximeihd')
 let st = '@123hb#*^&xiMEI99'
 let ximeikey = '',id = '',uid='',tid='',name=''
-!(async () => {
-  if (typeof $request !== "undefined") {
-    await ximeick()
+let max = 40
+let min = 12
+
+if ($.isNode()) {
+   if (process.env.XIMEI_URL && process.env.XIMEI_URL.indexOf('#') > -1) {
+   ximeiurl = process.env.XIMEI_URL.split('#');
+   console.log(`您选择的是用"#"隔开\n`)
+  }
+  else if (process.env.XIMEI_URL && process.env.XIMEI_URL.indexOf('\n') > -1) {
+   ximeiurl = process.env.XIMEI_URL.split('\n');
+   console.log(`您选择的是用换行隔开\n`)
+  } else {
+   ximeiurl = process.env.XIMEI_URL.split()
+  };
+  if (process.env.XIMEI_HD && process.env.XIMEI_HD.indexOf('#') > -1) {
+   ximeihd = process.env.XIMEI_HD.split('#');
+   console.log(`您选择的是用"#"隔开\n`)
+  }
+  else if (process.env.XIMEI_HD && process.env.XIMEI_HD.indexOf('\n') > -1) {
+   ximeihd = process.env.XIMEI_HD.split('\n');
+   console.log(`您选择的是用换行隔开\n`)
+  } else {
+   ximeihd = process.env.XIMEI_HD.split()
+  };
+
    
+Object.keys(lgyyurl).forEach((item) => {
+        if (lgyyurl[item]) {
+          lgyyurlArr.push(lgyyurl[item])
+        }
+    });
+    Object.keys(lgyyhd).forEach((item) => {
+        if (lgyyhd[item]) {
+          lgyyhdArr.push(lgyyhd[item])
+        }
+    });
+
+  
+    console.log(`============ 脚本执行-国际标准时间(UTC)：${new Date().toLocaleString()}  =============\n`)
+    console.log(`============ 脚本执行-北京时间(UTC+8)：${new Date(new Date().getTime() + 8 * 60 * 60 * 1000).toLocaleString()}  =============\n`)   
   } else {ximeiurlArr.push($.getdata('ximeiurl'))
     ximeihdArr.push($.getdata('ximeihd'))
     let ximeicount = ($.getval('ximeicount') || '1');
